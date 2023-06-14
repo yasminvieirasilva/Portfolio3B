1291a54a278a8b1d6ffef7c4b6bb58f36476f638
Yasmin Vieira da Silva <102532302+yasminvieirasilva@users.noreply.github.com>
2023-06-14 09:28:41 -0300

    Atv1.js

--- LogicaJS/Atv1.js
+++ LogicaJS/Atv1.js
@@ -8,3 +8,21 @@ else
 }
 console.log(maiorMenor (2,6))
 
+//2
+function vida(idade) {
+
+    const anos = 365
+    var dias = idade * anos  
+    return dias
+}
+    
+  console.log(vida (10))
+
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
