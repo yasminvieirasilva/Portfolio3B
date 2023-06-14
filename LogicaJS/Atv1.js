8f83289fbee09fcde88c71d74c85bcc77d7a0126
Yasmin Vieira da Silva <102532302+yasminvieirasilva@users.noreply.github.com>
2023-06-14 09:55:41 -0300

    Update Atv1.js

--- LogicaJS/Atv1.js
+++ LogicaJS/Atv1.js
@@ -19,6 +19,55 @@ function vida(idade) {
   console.log(vida (10))
 
 //3
+function mesAno (mes){
+switch (mes) {
+    case 1:
+        console.log('Janeiro')
+        break;
+    case 2:
+	    console.log( 'Fevereiro')
+        break;
+    case 3:
+        console.log("Março") 
+        break;
+
+        case 4:
+        console.log('Abril')
+        break;
+    case 5:
+	    console.log( 'Maio')
+        break;
+    case 6:
+        console.log("Junho") 
+        break;
+
+        case 7:
+        console.log('Julho')
+        break;
+    case 8:
+	    console.log( 'Agosto')
+        break;
+    case 9:
+        console.log("Setembro") 
+        break;
+
+        case 10:
+        console.log('Outubro')
+        break;
+    case 11:
+	    console.log( 'Novembro')
+        break;
+    case 12:
+        console.log("DEzembro") 
+        break;
+
+    default:
+        console.log("Este mes não existe")
+        break;
+}
+}
+mesAno (5)
+
 //4
 //5
 //6
