99764e06b7068dc6f9d4c943b31d3c1fe39d0c9e
Yasmin Vieira da Silva <102532302+yasminvieirasilva@users.noreply.github.com>
2023-07-12 09:30:44 -0300

    Create listaTelefonica.js

---
+++ AtvdAna/listaTelefonica.js
@@ -0,0 +1,24 @@
+function telefone (numero){
+switch (numero) {
+case 'Yasmin':
+        console.log('48 9176-4477')
+        break;
+    case 'José':
+	    console.log( '48 9111-1916')
+        break;
+    case 'Joice':
+        console.log("48 99153-5266") 
+        break;
+    case 'Olivia':
+        console.log('48 9198-2248')
+        break;
+    case 'Sabrina':
+	    console.log( '48 9156-9132')
+        break;
+   
+    default:
+        console.log("Este número não existe")
+        break;
+}
+}
+telefone ('Yasmin')
